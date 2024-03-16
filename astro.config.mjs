0e1b849108a3c362144858f09fdc0e403172702c
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-03-16 08:37:04 +0700

    Add @astrojs/partytown integration and Google tag (gtag.js)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,22 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, icon()],
+  integrations: [
+    tailwind(),
+    astroImageTools,
+    icon(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: "server",
   adapter: vercel(),
 });
